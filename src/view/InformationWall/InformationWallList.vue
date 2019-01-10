93980509084a301d176fb1a35d96dfef515ea250
王广开 <416266475@qq.com>
2019-01-10 15:34:50 +0800

    信息上墙

--- src/view/InformationWall/InformationWallList.vue
+++ src/view/InformationWall/InformationWallList.vue
@@ -1,8 +1,65 @@
 <template>
-
+  <div>
+    <div class="InformationWallList-head">
+      <span class="InformationWallList-head-title">信息上墙</span>
+      <button class="InformationWallList-head-btn" type="button">+ 发布</button>
+    </div>
+    <div class="InformationWallList-cnt">
+      <ol>
+        <li
+          class="InformationWallList-item"
+        >
+          <div class="InformationWallList-item-portrait">
+            <img src="@/assets/img/Bitmap@2x(4).png">
+          </div>
+          <div class="InformationWallList-item-cnt">
+            <p class="InformationWallList-item-head">
+              <span class="name">小区住户</span>
+              <span class="time">2018/10/12 16:23</span>
+            </p>
+            <p class="InformationWallList-item-inf">
+              老人8月12日与金沙滩风景区走丢身穿蓝色中山装，头带浅绿色帽子，家里万分着急，如有遇到的，请联系：18489293王女士，万分着急，谢谢，老人有高血压糖尿病，每日需要用药，走丢时身上没带任何药物。
+            </p>
+            <img
+              class="InformationWallList-item-img"
+              src="@/assets/img/Bitmap@2x(1).png"
+            >
+            <div class="InformationWallList-item-btm">
+              <i class="iconfont icon-xing"></i>
+              <span class="count">321个关注</span>
+            </div>
+          </div>
+        </li>
+        <li
+          class="InformationWallList-item"
+        >
+          <div class="InformationWallList-item-portrait">
+            <img src="@/assets/img/Bitmap@2x(4).png">
+          </div>
+          <div class="InformationWallList-item-cnt">
+            <p class="InformationWallList-item-head">
+              <span class="name">小区住户</span>
+              <span class="time">2018/10/12 16:23</span>
+            </p>
+            <p class="InformationWallList-item-inf">
+              老人8月12日与金沙滩风景区走丢身穿蓝色中山装，头带浅绿色帽子，家里万分着急，如有遇到的，请联系：18489293王女士，万分着急，谢谢，老人有高血压糖尿病，每日需要用药，走丢时身上没带任何药物。
+            </p>
+            <img
+              class="InformationWallList-item-img"
+              src="@/assets/img/Bitmap@2x(1).png"
+            >
+            <div class="InformationWallList-item-btm">
+              <i class="iconfont icon-xing"></i>
+              <span class="count">321个关注</span>
+            </div>
+          </div>
+        </li>
+      </ol>
+    </div>
+  </div>
 </template>
 <script>
-  export default {}
+  export default {};
 </script>
 <style>
 
