79feb0d2317d829c7b0ba25a2c1866c255fccf82
王广开 <416266475@qq.com>
2019-01-16 15:28:29 +0800

    支付步骤2

--- src/view/payment/PaymentDetail.vue
+++ src/view/payment/PaymentDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="PaymentDetail">
     <div class="PaymentDetail-head">
       <img src="@/assets/img/icon-property@2x.png">
       <p>{{type}}</p>
@@ -7,25 +7,35 @@
     <div class="PaymentDetail-address">
       <HPaper
         :headHandle="false"
-        :headTransition="true"
+        :headTransition="false"
       >
         <div class="PaymentDetail-cnt">
-          <label class="name">缴费地址</label>
-          <p class="val">{{address}}</p>
+          <p class="title">{{communityName}}</p>
+          <div class="PaymentDetail-checklist Payment-checklist">
+            <h-checklist
+              v-for="(item,i) in checkList"
+              :key="i"
+              :checkVal.sync="item.checked"
+            >
+              <div class="PaymentDetail-checklist-cnt">
+                <p class="PaymentDetail-checklist-cnt-total">
+                  <span class="name">{{item.room}}</span>
+                  <span class="val">{{item.money}}元</span>
+                </p>
+                <ul class="PaymentDetail-checklist-cnt-item-par">
+                  <li
+                    class="PaymentDetail-checklist-cnt-item"
+                    v-for="(year,i) in item.yearAmount"
+                    :key="i"
+                  >
+                    <span>{{year.value}}</span><span>{{year.money}}</span>
+                  </li>
+                </ul>
+              </div>
+            </h-checklist>
+          </div>
         </div>
       </HPaper>
-    </div>
-    <div class="PaymentDetail-checklist Payment-checklist">
-      <h-checklist
-        v-for="(item,i) in checkList"
-        :key="i"
-        :checkVal.sync="item.checked"
-      >
-        <div class="PaymentDetail-checklist-cnt">
-          <span>{{item.value}}</span>
-          <span>{{item.money}}元</span>
-        </div>
-      </h-checklist>
     </div>
     <footer class="PaymentDetail-footer">
       <div class="PaymentDetail-footer-total">
@@ -56,7 +66,7 @@ export default {
   data () {
     return {
       type: '物业费',
-      address: '龙道葡泊园小区 2号楼 3单元 202室',
+      communityName: '龙道葡泊园小区',
       form: {
         totalMoney: '3021.93'
       },
@@ -64,13 +74,34 @@ export default {
         {
           key: '1',
           checked: false,
-          value: '2019年物业费',
-          money: '3021.93'
+          room: '2号楼三单元202室',
+          money: '3021.93',
+          yearAmount: [
+            {
+              value: '2019年物业费',
+              money: '1568.00'
+            },
+            {
+              value: '2018年物业费',
+              money: '1568.00'
+            }
+          ]
         },
         {
           key: '2',
           checked: false,
-          value: '3021.93'
+          room: '2号楼三单元402室',
+          money: '4021.93',
+          yearAmount: [
+            {
+              value: '2019年物业费',
+              money: '1568.00'
+            },
+            {
+              value: '2018年物业费',
+              money: '1568.00'
+            }
+          ]
         }
       ],
       checkedList: []
