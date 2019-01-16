fab6f4d184d79d2968d891829b39ae9706edf7c3
王广开 <416266475@qq.com>
2019-01-16 16:08:44 +0800

    支付渠道改版

--- src/view/payment/PaymentChannel.vue
+++ src/view/payment/PaymentChannel.vue
@@ -1,13 +1,51 @@
 <template>
   <div>
     <div class="PaymentChannel-head">
-      <label class="PaymentChannel-prompt">支付金额：</label>
-      <p>
-        <span class="PaymentChannel-money">{{form.totalMoney}}</span>
-        <span class="PaymentChannel-moneyUnit">元</span>
-      </p>
+      <img src="@/assets/img/icon-property@2x.png">
+      <p class="PaymentChannel-prompt">物业费</p>
     </div>
+    <ol class="PaymentChannel-cnt">
+      <li
+        class="PaymentChannel-cnt-item"
+      >
+        <label class="name">应缴金额</label>
+        <span class="money">{{form.totalMoney}}</span>
+      </li>
+      <li
+        class="PaymentChannel-cnt-item"
+      >
+        <label class="name">缴费地址</label>
+        <div class="PaymentChannel-cnt-item-right">
+          <p
+            v-for="(item,i) in form.address"
+            :key="i"
+            class="val"
+          >{{item}}</p>
+        </div>
+      </li>
+      <li
+        class="PaymentChannel-cnt-item"
+      >
+        <label class="name">缴费项</label>
+        <div class="PaymentChannel-cnt-item-right">
+          <p
+            v-for="(item,i) in form.paymentItems"
+            :key="i"
+            class="val"
+          >{{item}}</p>
+        </div>
+      </li>
+      <li
+        class="PaymentChannel-cnt-item"
+      >
+        <label class="name">收款单位</label>
+          <span
+            class="val"
+          >{{form.company}}</span>
+      </li>
+    </ol>
     <div class="PaymentChannel-checklist">
+      <p class="PaymentChannel-checklist-head">支付渠道</p>
       <h-checklist
         v-for="(item,i) in checkList"
         :key="i"
@@ -44,7 +82,16 @@ export default {
   data () {
     return {
       form: {
-        totalMoney: '3021.93'
+        company: '大海物业',
+        totalMoney: '3021.93',
+        address: [
+          '龙道葡萄泊园小区2号楼3单元202室',
+          '龙道葡萄泊园小区2号楼3单元402室'
+        ],
+        paymentItems: [
+          '2017物业费',
+          '2018物业费'
+        ]
       },
       checkList: [
         {
