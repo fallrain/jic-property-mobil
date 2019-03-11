0b3952bd88e63bf4f888a37e35a242c4284b3f69
王广开 <41666475@qq.com>
2019-03-11 10:23:13 +0800

    未绑定页面

---
+++ src/view/nobind/Nobind.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="Nobind">
+    <div class="Nobind-img-par">
+      <img
+        class="Nobind-img"
+        src="@/assets/img/nobind.png"
+      >
+      <p class="Nobind-img-inf">您还没有绑定用户</p>
+    </div>
+    <h-button
+      class="Nobind-btn"
+      :width="264"
+      :height="39"
+      cnt="我要绑定"
+      @click.native="toBind"
+    ></h-button>
+  </div>
+</template>
+
+<script>
+import {HButton} from '@/components/common';
+
+export default {
+  name: 'Nobind',
+  components: {
+    HButton
+  },
+  methods: {
+    toBind () {
+      this.$router.push({
+        name: 'MyCommunityList'
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
